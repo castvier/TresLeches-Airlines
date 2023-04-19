e5a98dc9ff044dd75108fe84827dfa31bf75dce6
Jimwell Castillo <castvier@gmail.com>
2023-04-19 09:28:12 -0700

    add

--- airline/ui/FlightManagementUI.java
+++ airline/ui/FlightManagementUI.java
@@ -1,9 +1,7 @@
 package com.airline.ui;
 
 import com.airline.models.Airport;
-import com.airline.models.Flight; // Add this import
 import com.airline.management.FlightManagement;
-import com.airline.models.Airplane;
 
 
 import javafx.application.Application;
--- airline/ui/PassengerUI.java
+++ airline/ui/PassengerUI.java
@@ -1,7 +1,5 @@
 package com.airline.ui;
 
-import com.airline.models.Airplane;
-import com.airline.models.Flight;
 import com.airline.models.Passenger;
 import javafx.application.Application;
 import javafx.geometry.Insets;
