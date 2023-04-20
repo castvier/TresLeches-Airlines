729309350c0c055d3430635c51eb20bd680b2789
arielmvillegas <arielmarievillegas@outlook.com>
2023-04-19 20:51:39 -0700

    testing

--- airline/models/Passenger.java
+++ airline/models/Passenger.java
@@ -1,7 +1,9 @@
 package com.airline.models;
 
 // Passenger class manages personal data along with their seat assignments.
+
 public class Passenger {
+
 
     private String name; // name of passenger
     private final int age; // age final because can't be changed afterwards
@@ -13,30 +15,30 @@ public class Passenger {
         this.name = name; // setting passenger's name
         this.age = age; // age
         this.email = email; // email address
-        this.phoneNumber = phoneNumber; // phone number
+        this.phoneNumber = phoneNumber; // Set the passenger's phone number
     }
 
     public void setName(String name) {
-        this.name = name; // new name for the passenger
+        this.name = name; // Set a new name for the passenger
     }
 
     public String getName() {
-        return name; // getting the passenger's name
+        return name; // Get the passenger's name
     }
 
     public String getPhoneNumber() {
-        return phoneNumber; // passenger's phone number
+        return phoneNumber; // Get the passenger's phone number
     }
 
     public String getEmail() {
-        return email; // passenger's email
+        return email; // Get the passenger's email address
     }
 
     public String getSeatNumber() {
-        return seatNumber; // passenger's assigned seat
+        return seatNumber; // Get the passenger's assigned seat number
     }
 
     public void setSeatNumber(String seatNumber) {
-        this.seatNumber = seatNumber; // seat number for the passenger
+        this.seatNumber = seatNumber; // Set a new seat number for the passenger
     }
 }
