b1060a247ae9cd830eb297ca20993db81c2627ab
Gabe Moreno <gabe@sitrick.net>
2023-05-05 21:50:27 -0700

    Added Manager and Airport methods assigned

--- airline/models/ManagerTest.java
+++ airline/models/ManagerTest.java
@@ -1,95 +1,63 @@
-package com.airline.models;
+package com.airline.test;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.airline.models.Flight;
+import com.airline.models.Manager;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class ManagerTest {
 
-class ManagerTest {
     private Manager manager;
-    private Flight flight1, flight2, flight3;
 
     @BeforeEach
     void setUp() {
-        // Initialize the Manager instance with some flights
-        flight1 = new Flight("ABC123", "New York", "Los Angeles", "10:00", "14:00");
-        flight2 = new Flight("DEF456", "Chicago", "Denver", "12:00", "15:00");
-        flight3 = new Flight("GHI789", "San Francisco", "Seattle", "11:00", "13:00");
-
+        // Set up a new Manager instance with a list of flights
         ArrayList<Flight> flights = new ArrayList<>();
-        flights.add(flight1);
-        flights.add(flight2);
-        flights.add(flight3);
-
+        flights.add(new Flight("ABC123", "New York", "London", "10:00", "18:00"));
+        flights.add(new Flight("DEF456", "London", "Paris", "12:00", "14:00"));
         manager = new Manager(flights);
     }
 
     @Test
-    void addFlight() {
-        // Add a new flight to the Manager instance
-        Flight flight4 = new Flight("JKL012", "Miami", "Orlando", "9:00", "10:00");
-        manager.addFlight(flight4);
-
-        // Assert that the Manager instance now contains the new flight
-        assertTrue(manager.getFlights().contains(flight4));
+    void testAddFlight() {
+        // Test adding a new flight to the Manager's list of flights
+        Flight newFlight = new Flight("GHI789", "Paris", "Madrid", "15:00", "17:00");
+        manager.addFlight(newFlight);
+        Assertions.assertTrue(manager.getFlights().contains(newFlight));
     }
 
     @Test
-    void removeFlight() {
-        // Remove an existing flight from the Manager instance
-        manager.removeFlight(flight1.getNumber());
-
-        // Assert that the Manager instance no longer contains the removed flight
-        assertFalse(manager.getFlights().contains(flight1));
+    void testRemoveFlight() {
+        // Test removing a flight from the Manager's list of flights
+        String flightNumber = "ABC123";
+        manager.removeFlight(flightNumber);
+        Assertions.assertTrue(manager.getFlights().stream().noneMatch(flight -> flight.getNumber().equals(flightNumber)));
     }
 
     @Test
-    void getFlights() {
-        // Get the list of flights managed by the Manager instance
-        ArrayList<Flight> flights = manager.getFlights();
-
-        // Assert that the list contains all of the flights that were added in setUp()
-        assertTrue(flights.contains(flight1));
-        assertTrue(flights.contains(flight2));
-        assertTrue(flights.contains(flight3));
+    void testSearchFlightsByNumber() {
+        // Test searching for a flight by flight number
+        String flightNumber = "DEF456";
+        Flight expectedFlight = manager.getFlights().stream().filter(flight -> flight.getNumber().equals(flightNumber)).findFirst().orElse(null);
+        Assertions.assertEquals(expectedFlight, manager.searchFlightsByNumber(flightNumber).orElse(null));
     }
 
     @Test
-    void setFlights() {
-        // Set a new list of flights for the Manager instance
-        ArrayList<Flight> newFlights = new ArrayList<>();
-        newFlights.add(new Flight("MNO345", "Dallas", "Houston", "8:00", "9:00"));
-        newFlights.add(new Flight("PQR678", "Boston", "Washington DC", "9:30", "11:00"));
-        manager.setFlights(newFlights);
-
-        // Assert that the Manager instance now contains only the new flights
-        assertFalse(manager.getFlights().contains(flight1));
-        assertFalse(manager.getFlights().contains(flight2));
-        assertFalse(manager.getFlights().contains(flight3));
-
-        assertTrue(manager.getFlights().contains(newFlights.get(0)));
-        assertTrue(manager.getFlights().contains(newFlights.get(1)));
+    void testUpdateFlight() {
+        // Test updating the departure and arrival times for a flight
+        String flightNumber = "ABC123";
+        Flight flight = manager.getFlights().stream().filter(f -> f.getNumber().equals(flightNumber)).findFirst().orElse(null);
+        if (flight != null) {
+            String newDepartureTime = "11:00";
+            String newArrivalTime = "19:00";
+            manager.updateFlight(flight, newDepartureTime, newArrivalTime);
+            Assertions.assertEquals(newDepartureTime, flight.getDepartureTime());
+            Assertions.assertEquals(newArrivalTime, flight.getArrivalTime());
+        } else {
+            Assertions.fail("Flight not found");
+        }
     }
-
-    @Test
-    void searchFlightsByNumber() {
-        // Search for an existing flight by its number
-        Optional<Flight> result = manager.searchFlightsByNumber(flight2.getNumber());
-
-        // Assert that the search returns the correct flight as an Optional object
-        assertTrue(result.isPresent());
-        assertEquals(flight2, result.get());
-
-        // Search for a non-existent flight by a fake number
-        Optional<Flight> fakeResult = manager.searchFlightsByNumber("FAKE000");
-
-        // Assert that the search returns an empty Optional object
-        assertTrue(fakeResult.isEmpty());
-    }
-
-    @Test
-    void updateFlight() {
-        // Update an existing flight with new departure and
+}
